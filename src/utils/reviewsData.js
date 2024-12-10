0e91502ad0c2088c44e7bb0a4e3bc8cb4613c636
oren0115 <nyomana369@gmail.com>
2024-12-10 19:10:42 +0700

    update home page

--- src/utils/reviewsData.js
+++ src/utils/reviewsData.js
@@ -3,31 +3,19 @@ const reviewsData = [
     id: 1,
     name: "John Doe",
     comment: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
-<<<<<<< HEAD
     photo: "./src/assets/icons/Group 5.png",
-=======
-    photo: "./src/assets/icons/Group 5.png", // Ganti dengan URL foto asli
->>>>>>> ebf60b72b3c14553faca837751e90fe20cd622a7
   },
   {
     id: 2,
     name: "Jane Smith",
     comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-<<<<<<< HEAD
     photo: "./src/assets/icons/Group 5.png",
-=======
-    photo: "./src/assets/icons/Group 5.png", // Ganti dengan URL foto asli
->>>>>>> ebf60b72b3c14553faca837751e90fe20cd622a7
   },
   {
     id: 3,
     name: "Michael Johnson",
     comment: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
-<<<<<<< HEAD
     photo: "./src/assets/icons/Group 5.png",
-=======
-    photo: "./src/assets/icons/Group 5.png", // Ganti dengan URL foto asli
->>>>>>> ebf60b72b3c14553faca837751e90fe20cd622a7
   },
 ];
 
